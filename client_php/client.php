ebc6135a09ee84a7d3f0a2333bd35a73378ef7e1
hetal <hetao@hetao.name>
2019-10-23 11:46:07 +0800

    autoload

--- client_php/client.php
+++ client_php/client.php
@@ -1,9 +1,15 @@
 <?php
-require_once("vendor/autoload.php");
-require_once("../proto/php/GPBMetadata/User/Info/Info.php");
-require_once("../proto/php/Info/LoginRequest.php");
-require_once("../proto/php/Info/LoginResponse.php");
-require_once("../proto/php/Info/InfoClient.php");
+define("ROOT",dirname(__FILE__)."/");
+define("ROOT_PROTO_LIB",ROOT."../proto/php/");
+
+require_once(ROOT."vendor/autoload.php");
+
+//注册自己的类
+spl_autoload_register(function($class){
+	$root = ROOT_PROTO_LIB.str_replace("\\","/",$class).".php";
+	require_once($root);
+});
+
 try{
 	$client = new Info\InfoClient("127.0.0.1:50000",[
 			'credentials' => Grpc\ChannelCredentials::createInsecure()
@@ -15,6 +21,26 @@ try{
 	list($status,$r) = $client->login($request)->wait();
 	var_dump($status);
 	print_r($r);
+
+	$request = new Info\LogoutRequest();
+	list($status,$r) = $client->logout($request)->wait();
+	var_dump($status);
+	print_r($r);
+
+	$client = new Profile\ProfileClient("127.0.0.1:50000",[
+			'credentials' => Grpc\ChannelCredentials::createInsecure()
+	]);
+
+	$request = new Profile\GetRequest();
+	list($status,$r) = $client->get($request)->wait();
+	var_dump($status);
+	print_r($r);
+
+	$request = new Profile\UpdateRequest();
+	list($status,$r) = $client->update($request)->wait();
+	var_dump($status);
+	print_r($r);
+
 	$data = get("http://127.0.0.1:50001/v1/user/profile/get");
 	echo $data;
 	echo "\n";
